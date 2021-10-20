f0cfeff7a909a9270ed83ac9b189072aeb835b64
Taka <amano.takahisa@gmail.com>
2021-10-21 01:19:44 +0200

    initialize class and methods

--- decostr/decostr.py
+++ decostr/decostr.py
@@ -1,1 +1,16 @@
-"""Main module."""
+#!/usr/bin/env python
+
+ESC = '\033'
+
+
+class DecoStr:
+    def __init__(self, string: str):
+        self.string = string
+
+    def bold(self):
+        self.string = f'{ESC}[1m{self.string}{ESC}[21m'
+        return self
+
+    def underline(self):
+        self.string = f'{ESC}[4m{self.string}{ESC}[24m'
+        return self
