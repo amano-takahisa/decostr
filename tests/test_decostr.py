91085dd525a69a4a0335c966dd3cb496d97e9044
Taka <amano.takahisa@gmail.com>
2021-10-24 21:45:10 +0200

    #1: add decostring property and use .string to store original strings.

--- tests/test_decostr.py
+++ tests/test_decostr.py
@@ -5,20 +5,11 @@
 import pytest
 
 
-from decostr import decostr
+from decostr.decostr import DecoStr
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+class Test_DecoStr:
+    def test_decostr(self):
+        out = DecoStr('sample text').bold().underline()
+        assert out.decostring == '\x1b[4m\x1b[1msample text\x1b[21m\x1b[24m'
+        assert out.string == 'sample text'
